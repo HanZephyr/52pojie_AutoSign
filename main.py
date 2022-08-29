9182b4283f313185945360afa91d05713043af08
冬酒暖阳 <50990263+allwaysLove@users.noreply.github.com>
2022-08-29 15:10:18 +0800

    优化代码

--- main.py
+++ main.py
@@ -11,19 +11,16 @@ def sign(cookie):
         "ContentType": "text/html;charset=gbk",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
     }
-    requests.session().put(
-        "https://www.52pojie.cn/home.php?mod=task&do=apply&id=2", headers=headers
-    )
-    fa = requests.session().put(
+    res = requests.session().put(
         "https://www.52pojie.cn/home.php?mod=task&do=draw&id=2", headers=headers
     )
-    fb = BeautifulSoup(fa.text, "html.parser")
-    fc = fb.find("div", id="messagetext").find("p").text
-    if "您需要先登录才能继续本操作" in fc:
+    soup = BeautifulSoup(res.text, "html.parser")
+    text = soup.find("div", id="messagetext").find("p").text
+    if "您需要先登录才能继续本操作" in text:
         result += "Cookie 失效"
-    elif "恭喜" in fc:
+    elif "恭喜" in text:
         result += "签到成功"
-    elif "不是进行中的任务" in fc:
+    elif "不是进行中的任务" in text:
         result += "今日已签到"
     else:
         result += "签到失败"
